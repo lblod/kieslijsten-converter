43d3ca67a2e632081cb449d7a04f49d0d36a9eaa
Niels Vandekeybus <progster@gmail.com>
2018-10-30 17:07:05 +0100

    convert kandidaten

--- converter/convert-to-ttl.rb
+++ converter/convert-to-ttl.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# coding: utf-8
 
 require 'csv'
 require 'linkeddata'
@@ -21,6 +22,7 @@ class MandatenDb
   MANDAAT = RDF::Vocabulary.new("http://data.vlaanderen.be/ns/mandaat#")
   BESLUIT = RDF::Vocabulary.new("http://data.vlaanderen.be/ns/besluit#")
   EXT = RDF::Vocabulary.new("http://mu.semte.ch/vocabularies/ext/")
+  ADMS = RDF::Vocabulary.new('http://www.w3.org/ns/adms#')
   BASE_IRI='http://data.lblod.info/id'
 
   attr_reader :client, :log
@@ -90,6 +92,98 @@ class MandatenDb
     graph << [iri, MANDAAT.steltSamen, stelt_samen]
     [iri, graph]
   end
+
+  def find_person(rrn)
+    result = query(%(
+      PREFIX adms:<http://www.w3.org/ns/adms#>
+      PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
+      SELECT ?person
+      WHERE {
+        ?person adms:identifier ?identifier.
+        ?identifier skos:notation "#{rrn}".
+      }
+
+    ))
+    if result.size == 1
+      result[0][:person]
+    else
+      nil
+    end
+  end
+
+  def birthdate(date)
+    result = query(%(
+      PREFIX persoon: <http://data.vlaanderen.be/ns/persoon#>
+      SELECT ?birthdate
+      WHERE {
+        ?birthdate a persoon:Geboorte;
+                   persoon:datum "#{date}"^^xsd:date
+      }
+    ))
+    if (result.size == 1)
+      return [result[0][:birthdate], RDF::Repository.new]
+    else
+      uuid = SecureRandom.uuid
+      triples = RDF::Repository.new
+      iri = RDF::URI.new("#{BASE_IRI}/geboorte/#{uuid}")
+      triples << [iri, RDF.type, PERSOON.Geboorte ]
+      triples << [iri, MU.uuid, uuid ]
+      triples << [iri, PERSOON.datum, RDF::Literal.new(date)]
+      return [iri, triples]
+    end
+
+  end
+
+  def geslacht(g)
+    if g == 'F'
+      RDF::URI.new('http://publications.europa.eu/resource/authority/human-sex/FEMALE')
+    elsif g == 'M'
+      RDF::URI.new('http://publications.europa.eu/resource/authority/human-sex/MALE')
+    else
+      raise "ongeldige geslachtscode"
+    end
+  end
+
+  def create_person( rrn:, voornaam:, achternaam:, geslacht: , geboortedatum: )
+    sensitive_triples = RDF::Repository.new
+    triples = RDF::Repository.new
+    person_uuid = SecureRandom.uuid
+    person = RDF::URI.new("#{BASE_IRI}/personen/#{person_uuid}")
+    identifier_uuid = SecureRandom.uuid
+    identifier = RDF::URI.new("#{BASE_IRI}/identificatoren/#{identifier_uuid}")
+    sensitive_triples << [ person, ADMS.identifier, identifier ]
+    sensitive_triples << [ identifier, RDF.type, ADMS.Identifier ]
+    sensitive_triples << [ identifier, SKOS.notation, rrn ]
+    sensitive_triples << [ identifier, MU.uuid, identifier_uuid ]
+    triples << [ person, RDF.type, PERSON.Person ]
+    triples << [ person, MU.uuid, person_uuid ]
+    if voornaam
+      triples << [ person, PERSOON.gebruikteVoornaam , voornaam ]
+    end
+    triples << [ person, FOAF.familyName , achternaam ]
+    if geboortedatum
+      (birthdate_iri, birthdate_triples) = birthdate(geboortedatum)
+      triples << [ person, PERSOON.heeftGeboorte, birthdate_iri ]
+      triples << birthdate_triples
+    end
+    triples << [ person, PERSOON.geslacht, geslacht(geslacht) ]
+    [person, triples, sensitive_triples]
+  end
+
+  def create_resultaat(persoon:,lijst:,naamstemmen:,gevolg:,rangorde:)
+    triples = RDF::Repository.new
+    uuid = SecureRandom.uuid
+    iri = RDF::URI.new("#{BASE_IRI}/verkiezingsresultaten/#{uuid}")
+    triples << RDF.Statement(iri, RDF.type, MANDAAT.Verkiezingsresultaat)
+    triples << RDF.Statement(iri, MU.uuid, uuid)
+    triples << RDF.Statement(iri, MANDAAT.aantalNaamstemmen, naamstemmen)
+    if rangorde
+      triples << RDF.Statement(iri, MANDAAT.plaatsRangorde, rangorde)
+    end
+    triples << RDF.Statement(iri, MANDAAT.isResultaatVan, persoon)
+    triples << RDF.Statement(iri, MANDAAT.isResultaatVoor, lijst)
+    [iri, triples]
+  end
 end
 
 class Converter
@@ -136,11 +230,74 @@ class Converter
     end
   end
 
-  def parse_kandidaten
+  def int(i)
+    begin
+      Integer(i)
+    rescue
+      0
+    end
+  end
+
+  def gevolg(row)
+    if int(row['verkozen']) > 0
+      RDF::URI.new('http://data.vlaanderen.be/id/concept/VerkiezingsresultaatGevolgCode/89498d89-6c68-4273-9609-b9c097727a0f')
+    elsif int(row['opvolger']) > 0
+      RDF::URI.new('http://data.vlaanderen.be/id/concept/VerkiezingsresultaatGevolgCode/4c713f09-1317-4860-bbbd-e8f7dfd78a2f')
+    else
+      RDF::URI.new('http://data.vlaanderen.be/id/concept/VerkiezingsresultaatGevolgCode/dc8169a2-0e20-495d-9e01-30ccc83422b8')
+    end
+  end
+
+  def rangorde(row)
+    if int(row['verkozen']) > 0
+      Integer(row['verkozen'])
+    elsif int(row['opvolger']) > 0
+      Integer(row['opvolger'])
+    else
+      nil
+    end
+  end
+
+  def parse_kandidaten(lijsten)
+    wait_for_db
     rrn_graph = RDF::Repository.new
     write_ttl_to_file('personen') do |repository|
-      read_csv(File.join(input_path,'kandidaten.csv')) do |row|
-        
+      read_csv(File.join(input_path,'kandidaten.csv')) do |index, row|
+        persoon = mdb.find_person(row['RR'])
+        unless persoon
+          begin
+            geboortedatum = Date.strptime(row["geboortedatum"], "%m/%d/%Y")
+          rescue StandardError => e
+            log.info "invalid date #{row["geboortedatum"]}"
+            log.info row.inspect
+            geboortedatum = nil
+          end
+          begin
+          ( persoon, triples, sensitive_triples ) = mdb.create_person( rrn:row['RR'],
+                                                                       voornaam: row['Rrvoornaam'],
+                                                                       achternaam: row['RRachternaam'],
+                                                                       geslacht: row['geslacht'],
+                                                                       geboortedatum: geboortedatum
+                                                                     )
+          rrn_graph << sensitive_triples
+          repository.write(triples.dump(:ttl))
+          lijst = lijsten["#{row["kieskring"]}-#{row["lijstnr"]}"]
+          gevolg = gevolg(row)
+          rangorde = rangorde(row)
+          (resultaat, triples ) = mdb.create_resultaat(persoon: persoon,
+                                                       lijst: lijst,
+                                                       naamstemmen: Integer(row["naamstemmen"]),
+                                                       gevolg: gevolg,
+                                                       rangorde: rangorde
+                                                      )
+          triples << [ lijst, ::MandatenDb::MANDAAT.heeftKandidaat, persoon ]
+          repository.write(triples.dump(:ttl))
+          rescue StandardError => e
+            log.error e
+            log.error "failed to parse row #{index}"
+            log.info row.inspect
+          end
+        end
       end
     end
     write_ttl_to_file('personen-sensitive') do |repository|
@@ -149,7 +306,9 @@ class Converter
   end
 
   def parse_kieslijsten(lijsttype_iri, bestuursorgaan_iri, orgaan_start_datum)
+    wait_for_db
     verkiezing_cache = {}
+    kieslijsten = {}
     write_ttl_to_file('rechtstreekse-verkiezingen-en-kandidatenlijsten') do |repository|
       read_csv(File.join(input_path,'kandidaten.csv')) do |index, row|
         gemeentenaam = row["kieskring"]
@@ -157,7 +316,7 @@ class Converter
         orgaantype = RDF::URI.new(bestuursorgaan_iri)
         begin
           orgaan = mdb.bestuursorgaan_voor_gemeentenaam(gemeentenaam, orgaantype, orgaan_start_datum )
-          date =  DateTime.strptime(row["datum"], input_date_format)
+          date =  Date.strptime(row["datum"], input_date_format)
           if verkiezing_cache[orgaan]
             verkiezing = verkiezing_cache[orgaan]
           else
@@ -166,12 +325,14 @@ class Converter
             verkiezing_cache[orgaan]=verkiezing
           end
           ( lijst, graph ) = mdb.create_kandidatenlijst(lijstnaam: row["lijst"], lijstnr: row["lijstnr"], lijsttype: lijsttype, behoortTot: verkiezing)
+          kieslijsten["#{gemeentenaam}-#{row["lijstnr"]}"]=lijst
           repository.write(graph.dump(:ttl))
         rescue StandardError => e
           log.warn "skipping row #{index}: #{e.message}"
         end
       end
     end
+    kieslijsten
   end
 
   def write_ttl_to_file(name)
@@ -181,7 +342,7 @@ class Converter
       yield output
       output.write "# finished #{name} at #{DateTime.now}"
       output.close
-      FileUtils.copy(output, File.join(output_path,"#{name}.ttl"))
+      FileUtils.copy(output, File.join(output_path,"#{DateTime.now.strftime("%Y%m%dT%H%M%S")}-#{name}.ttl"))
       output.unlink
     rescue StandardError => e
       log.error(e)
@@ -225,5 +386,6 @@ converter = Converter.new(
   log_level: ENV['LOG_LEVEL']
 )
 converter.run_data_transforms
-converter.parse_kieslijsten(ENV['KANDIDATENLIJST_TYPE_IRI'], ENV['BESTUURSORGAAN_TYPE_IRI'], ENV['ORGAAN_START_DATUM'])
+kieslijsten = converter.parse_kieslijsten(ENV['KANDIDATENLIJST_TYPE_IRI'], ENV['BESTUURSORGAAN_TYPE_IRI'], ENV['ORGAAN_START_DATUM'])
+converter.parse_kandidaten(kieslijsten)
 
